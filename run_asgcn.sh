94f7449b21db2e11637b8ec431aa2dacc8344bb8
Akuchi <bin.liang@stu.hit.edu.cn>
2021-11-22 16:47:34 +0800

    Delete run_asgcn.sh

--- run_asgcn.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#########################################################################
-# File Name: run.sh
-# Author: BinLiang
-# mail: 18b951033@stu.hit.edu.cn 
-# Created Time: Tue 26 May 2020 02:44:41 PM CST
-#########################################################################
-
-python3 train.py --model_name asgcn --dataset rest14 --save True
