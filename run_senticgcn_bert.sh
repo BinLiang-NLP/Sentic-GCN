fbda5b17ae562baec4ace4f2137d7e1205b79283
Akuchi <bin.liang@stu.hit.edu.cn>
2021-12-03 21:13:51 +0800

    update bert files

--- run_senticgcn_bert.sh
+++ run_senticgcn_bert.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=1 python3 train_bert.py --model_name senticgcn_bert --dataset rest14 --save True --learning_rate 2e-5 --seed 39 --batch_size 16 --hidden_dim 768
+CUDA_VISIBLE_DEVICES=1 python train_bert.py --model_name senticgcn_bert --dataset rest15 --lr 2e-5 --seed 39 --batch_size 16
